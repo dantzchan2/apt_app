beb5607b2c471f9a75183b64049469b165739e8d
dantzchan2 <dantzchan@gmail.com>
2025-08-14 20:56:47 +0900

    Update check_user.mjs

--- check_user.mjs
+++ check_user.mjs
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://ibedovvkyvgfsaetsdab.supabase.co';
-const supabaseServiceKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImliZWRvdnZreXZnZnNhZXRzZGFiIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc0NzEwNDk4NCwiZXhwIjoyMDYyNjgwOTg0fQ.l9m24AXh0ppwtdKsh7Dy5djMrYcTT93hsir-QljjyVs';
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
+const supabaseServiceKey = process.env.SUPABASE_SERVICE_ROLE_KEY;
 
 const supabase = createClient(supabaseUrl, supabaseServiceKey);
 
